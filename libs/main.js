b086db30e5452621742e5916679b929cceb0aa52
gaominghlwrjyb <gaominghlwrjyb@bbktel.com>
2018-12-13 19:57:41 +0800

    simple v-dom demo

--- libs/main.js
+++ libs/main.js
@@ -1,7 +1,1 @@
 var el = require('./element')
-
-var ul = el('ul', {id: 'list'}, [
-  el('li', {class: 'item'}, ['item 1']),
-  el('li', {class: 'item'}, ['item 2']),
-  el('li', {class: 'item'}, ['item 3'])
-])
