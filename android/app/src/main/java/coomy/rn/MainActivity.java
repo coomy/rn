9056bc0e957eaecf22d56b5422436e240229d963
gaominghlwrjyb <gaominghlwrjyb@bbktel.com>
2018-12-14 18:41:29 +0800

    add android j2v8 support code

--- android/app/src/main/java/coomy/rn/MainActivity.java
+++ android/app/src/main/java/coomy/rn/MainActivity.java
@@ -4,9 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.eclipsesource.v8.JavaVoidCallback;
+import com.eclipsesource.v8.Releasable;
 import com.eclipsesource.v8.V8;
+import com.eclipsesource.v8.V8Array;
+import com.eclipsesource.v8.V8Object;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String getFileContents(final File file) throws IOException {
+        final InputStream inputStream = new FileInputStream(file);
+        final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        boolean done = false;
+
+        while (!done) {
+            final String line = reader.readLine();
+            done = (line == null);
+
+            if (line != null) {
+                stringBuilder.append(line);
+            }
+        }
+
+        reader.close();
+        inputStream.close();
+
+        return stringBuilder.toString();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +53,23 @@ public class MainActivity extends AppCompatActivity {
                 "var world = 'world' \n" +
                  "hello.concat(world).length"
         );
+
+        JavaVoidCallback callback = new JavaVoidCallback() {
+            @Override
+            public void invoke(V8Object v8Object, V8Array v8Array) {
+                if (v8Array.length() > 0) {
+                    Object arg1 = v8Array.get(0);
+                    Log.e("CoomyRn", "js callback args : " + arg1);
+                    if (arg1 instanceof Releasable) {
+                        ((Releasable) arg1).release();
+                    }
+                }
+            }
+        };
+
+        runtime.registerJavaMethod(callback, "androidCallback");
+        runtime.executeScript("androidCallback('hello j2v8')");
+
         Log.e("CoomyRn", "JS result is : " + result);
         runtime.release();
     }
