01b7da3049553b475f0af1b23d2c1907b5e4dba0
gaominghlwrjyb <gaominghlwrjyb@bbktel.com>
2019-01-05 18:06:18 +0800

    改进j2v8为多线程模式ing...

--- android/app/src/main/java/coomy/rn/J2V8Engine.java
+++ android/app/src/main/java/coomy/rn/J2V8Engine.java
@@ -1,5 +1,10 @@
 package coomy.rn;
 
+import android.os.Handler;
+import android.os.HandlerThread; // TODO:android
+import android.os.Looper;
+import android.os.Message;
+
 import com.eclipsesource.v8.Releasable;
 import com.eclipsesource.v8.V8;
 import com.google.gson.Gson;
@@ -9,12 +14,45 @@ import java.io.InputStream;
 
 public class J2V8Engine {
 
-
+    private static String _TAG_ = "J2V8";
     private static J2V8Engine sInstance = null;
     private String mJsFramework = "";
     private V8 mRuntime = null;
 
+    private HandlerThread mThread = new HandlerThread(_TAG_);
+    private Handler mHandler = null;
+    private Message mMessage = null;
+
     private boolean mIsInitialized = false;
+
+    public class J2V8ThreadHandler extends Handler {
+
+        public static final int MSG_INIT = 1;
+        public static final int MSG_RENDER = 2;
+
+        J2V8ThreadHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_INIT:
+                    mRuntime = V8.createV8Runtime();
+                    mJsFramework = loadJsFile("util.js") + "\r\n" + loadJsFile("element.js");
+
+                    Object o = mRuntime.executeScript(mJsFramework);
+                    if (o instanceof Releasable) {
+                        ((Releasable) o).release();
+                    }
+
+                    mIsInitialized = true;
+                    break;
+                case MSG_RENDER:
+                    break;
+            }
+        }
+    }
 
     public static J2V8Engine getInstance() {
         if (sInstance == null) {
@@ -33,20 +71,28 @@ public class J2V8Engine {
 
     // must called
     private void initial() {
-        mRuntime = V8.createV8Runtime();
-        mJsFramework = loadJsFile("util.js") + "\r\n" + loadJsFile("element.js");
+        if (mThread == null)
+            mThread = new HandlerThread(_TAG_);
 
-        Object o = mRuntime.executeScript(mJsFramework);
-        if (o instanceof Releasable) {
-            ((Releasable) o).release();
-        }
+        if (!mThread.isAlive())
+            mThread.start();
 
-        mIsInitialized = true;
+        mHandler = new J2V8ThreadHandler(mThread.getLooper());
+
+        mMessage = mHandler.obtainMessage();
+        mMessage.what = J2V8ThreadHandler.MSG_INIT;
+        mHandler.sendMessage(mMessage);
     }
 
     public Element make(String scriptFile) {
         if (!mIsInitialized)
             initial();
+
+//        mMessage = mHandler.obtainMessage();
+//        mMessage.what = J2V8ThreadHandler.MSG_RENDER;
+//        mMessage
+//        mHandler.sendMessage(mMessage);
+        // 带返回值的怎么玩？？？
 
         String res = mRuntime.executeStringScript(loadJsFile(scriptFile));
 
